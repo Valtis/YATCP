b4092e67722b2e08d44f9715d1112dc5a22a9bc3
Erkka Kääriä <erkka.kaaria@outlook.com>
2019-07-20 22:35:04 +0300

    ICE-macro: Show file/line info more prominently
    While the panic!()-macro shows line and file already, showing
    them in a more prominent way makes debugging easier.

--- src/ice/mod.rs
+++ src/ice/mod.rs
@@ -3,18 +3,23 @@ macro_rules! ice {
     ($fmt:expr) => (
       {
        eprintln!(
-            "\n{}: {}\n\nThis is a bug in the compiler, not in the source file.\n",
+            "\n{}: {}\n\nIssue occurred in file {} at line {}\n\nThis is a bug in the compiler, not in the source file.\n",
             ansi_term::Colour::Red.bold().paint("Internal compiler error").to_string(),
-            $fmt);
+            $fmt,
+            file!(),
+            line!());
           panic!("Stop");
       }
     );
     ($fmt:expr, $($args:tt)+) => (
       {
         eprintln!(
-          "\n{}: {}\n\nThis is a bug in the compiler, not in the source file.\n",
+          "\n{}: {}\n\nIssue occurred in file {} at line {}\n\nThis is a bug in the compiler, not in the source file.\n",
           ansi_term::Colour::Red.bold().paint("Internal compiler error").to_string(),
-          format!($fmt, $($args)+));
+          format!($fmt, $($args)+),
+          file!(),
+          line!(),
+          );
         panic!("Stop");
       }
     )
@@ -27,9 +32,12 @@ macro_rules! ice_if {
       {
        if $condition {
            eprintln!(
-                "\n{}: {}\n\nThis is a bug in the compiler, not in the source file.\n",
+                "\n{}: {}\n\nIssue occurred in file {} at line {}\n\nThis is a bug in the compiler, not in the source file.\n",
                 ansi_term::Colour::Red.bold().paint("Internal compiler error").to_string(),
-                $fmt);
+                $fmt,
+                file!(),
+                line!(),
+                );
               panic!("Stop");
         }
       }
@@ -38,9 +46,12 @@ macro_rules! ice_if {
       {
         if $condition {
             eprintln!(
-              "\n{}: {}\n\nThis is a bug in the compiler, not in the source file.\n",
+              "\n{}: {}\n\nIssue occurred in file {} at line {}\n\nThis is a bug in the compiler, not in the source file.\n",
               ansi_term::Colour::Red.bold().paint("Internal compiler error").to_string(),
-              format!($fmt, $($args)+));
+              format!($fmt, $($args)+),
+              file!(),
+              line!(),
+              );
             panic!("Stop");
         }
       }
